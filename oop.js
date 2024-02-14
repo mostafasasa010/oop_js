cadbcaffa4585dd1903c4acff1791864c41cc6be
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-14 18:17:06 +0200

    Constractor Lesson 3

--- oop.js
+++ oop.js
@@ -1,28 +1,24 @@
-function Phone(serial) {
-  // this is The Created Object From The Constrcutor Function
-  console.log(this);
-  this.serial = `#${serial}`;
+function User(fName, lName, age, country) {
+  this.fName = fName;
+  this.lName = lName;
+  this.age = age;
+  this.country = country;
+  this.fullName = function () {
+    return `Full Name: ${this.fName} ${this.lName}`;
+  };
+  this.ageInDays = function () {
+    return `Age In Days: ${this.age * 365}`;
+  };
 }
+// User.country = "Egypt"; Wrong
 
-let phone1 = new Phone(123456);
-let phone2 = new Phone(528951);
-let phone3 = Phone(125698);
+let user1 = new User("Osama", "Elzero", 37, "Egypt");
+let user2 = new User("Ahmed", "Ali", 30, "KSA");
 
-console.log(phone1.serial);
-console.log(phone2.serial);
-console.log(window.serial);
-
-console.log(phone1 instanceof Phone);
-console.log(phone2 instanceof Phone);
-console.log(phone3 instanceof Phone);
-
-console.log(phone1.constructor === Phone);
-console.log(phone2.constructor === Phone);
-// console.log(phone3.constructor === Phone); // Error
-
-function sayHelloTo(someone) {
-  // someone => Parameter
-  return `Hello ${someone}`;
-}
-
-console.log(sayHelloTo("Osama")); // Osama => Argument
+console.log(user1);
+console.log(`Full Name: ${user1.fName} ${user1.lName}`);
+console.log(user1.fullName());
+console.log(user1.ageInDays());
+console.log(user2);
+console.log(user2.country);
+console.log(user2.ageInDays());
