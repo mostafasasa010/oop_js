0188442feda7c8fefdc849ccb7d625fdd601fc15
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 00:12:16 +0200

    The End

--- oop.js
+++ oop.js
@@ -1,41 +1,31 @@
 /*
-  Object.defineProperties & Trainings
+  Importants Notes And The End
+  [1] Arrow Functions Do Not Have a Prototype Property.
+  [2] You Can Use Objects Inside Constructor Freely
+  [3] f = function () {} ==== f() {}
 */
 
-const myObject = {
-  a: 1,
-  b: 2,
-};
+class User {
+  constructor(fName, lName, age, email) {
+    this.name = {
+      first: fName,
+      last: lName,
+    };
+    this.age = age;
+    this.email = email;
+  }
+  sayHello = function () {
+    return `Say Hello`;
+  };
+  sayHi() {
+    return `Say Hi`;
+  }
+}
 
-Object.defineProperty(myObject, "a", {
-  writable: false,
-  enumerable: false,
-  configurable: false,
-  value: 500,
-});
-
-Object.defineProperty(myObject, "c", {
-  value: 3,
-});
-
-myObject.d = 4;
-
-Object.defineProperties(myObject, {
-  e: {
-    value: 5,
-  },
-  f: {
-    value: 6,
-  },
-  g: {
-    value: 7,
-  },
-});
-
-console.log(myObject);
-console.log(Object.getOwnPropertyDescriptor(myObject, "a"));
-console.log(Object.getOwnPropertyDescriptor(myObject, "c"));
-console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
-
-console.log(Object.getOwnPropertyNames(myObject));
-console.log(Object.keys(myObject));
+let user1 = new User("Osama", "Elzero", 37, "o@nn.sa");
+console.log(user1.name.first);
+console.log(user1.name.last);
+console.log(user1.age);
+console.log(user1.email);
+console.log(user1.sayHello());
+console.log(user1.sayHi());
