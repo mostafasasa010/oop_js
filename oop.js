d779b7c36785f03d195d000fe2ab6c6d1f8afe33
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-15 23:26:48 +0200

    Classes Lesson 2

--- oop.js
+++ oop.js
@@ -1,11 +1,17 @@
 /*
   Class
+  Static Properties & Methods
 */
 
 class User {
-  constructor(name, email) {
+  // Static Properties
+  static counter = 0;
+
+  constructor(name, email, counter) {
     this.name = name;
     this.email = email;
+    this.counter = counter;
+    User.counter++;
   }
   sayHello() {
     return `Hello ${this.name}`;
@@ -13,9 +19,20 @@ class User {
   showEmail() {
     return `Your Email Is ${this.email}`;
   }
+
+  // Static Methods
+  static countObjects = function () {
+    return `${this.counter} Objects Created.`;
+  };
 }
 
-let user1 = new User("Osama", "o@nn.sa");
-let user2 = new User("Ahmed");
+let user1 = new User("Osama", "o@nn.sa", 2);
+let user2 = new User("Ahmed", "o@nn.sa", 2);
+let user3 = new User("Osama", "o@nn.sa", 2);
+let user4 = new User("Osama", "o@nn.sa", 2);
+// let user2 = User("Ahmed", "a@nn.sa"); // TypeError: Class constructor User
 
-console.log(user1);
+console.log(typeof User); // Function
+console.log(User === User.prototype.constructor); // True
+
+console.log(User.countObjects());
