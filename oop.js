0c839b5c9b5a59c5f715590d2391af245fae30a8
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-15 23:16:30 +0200

    Prototype Lesson 5

--- oop.js
+++ oop.js
@@ -1,39 +1,22 @@
 /*
-  Prototype
-  [1] Every Object Has A Prototype
-  [2] Prototype Chain Ends With Object.prototype
-  [2] In Javascript Function Is Object
+  Class
 */
 
-function User(name) {
-  /*
-    [1] Create Empty Object
-    [2] Assign The New Object To this Context
-    [3] New Object Created Prototype = Function Prototype
-    this = {};
-    this.__proto__ = User.__proto__
-  */
-  this.name = name;
-  /*
-    [4] Return The New Object
-    return this
-  */
-
-  // if (!(this instanceof User)) {
-  //   // throw new Error("Must Be Called With New Keyword");
-  //   console.log("Error");
-  // }
-
-  // ES6
-  if (!new.target) {
-    // throw new Error("Must Be Called With New Keyword");
-    console.log("Error");
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  sayHello() {
+    return `Hello ${this.name}`;
+  }
+  showEmail() {
+    return `Your Email Is ${this.email}`;
   }
 }
 
-let user1 = new User("Osama");
+let user1 = new User("Osama", "o@nn.sa");
 let user2 = new User("Ahmed");
-console.log(User.prototype);
-console.log(user1);
 
-let myArray = [1, 2, 3, 4];
+console.log(user1);
+console.log(user2);
